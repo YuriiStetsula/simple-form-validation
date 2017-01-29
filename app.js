3fb84c5d0f267f595e92c812b47d9cffc24fdd63
yurkkaa <s.yuri@ukr.net>
2017-01-29 16:38:54 +0200

    add angular version

--- app.js
+++ app.js
@@ -1,80 +1,29 @@
 
-(function(){
-
-
-var RegistrationForm = function (obj) {
-        
-        this.rootElem = document.querySelector(obj.rootElementClass);
-        this.form = obj.formName;
-        
-        this.pass = this.rootElem.querySelector(obj.passwordInputClass);
-        this.repeatPass = this.rootElem.querySelector(obj.passwordInputRepeatClass);
-        
-        this.handleEvent();
-
-    } 
-
-
-    RegistrationForm.prototype.handleEvent = function () {
-      var self = this;
-        for (var i = 0; i< this.form.length; i++){
- 
-                if ( this.form[i].type != "submit" ){
-
-                this.onFocus(this.form[i]);
-                this.invalid(this.form[i]);
-
-            }
-        
-            if (this.form[i].type === "password"){
-                this.form[i].addEventListener("blur", function (){
-
-                if( self.pass.value.length > 0 && self.repeatPass.value.length > 0){
-
-                        if(self.pass.value != self.repeatPass.value){
-                            self.pass.classList.add ("match_pass")
-                            self.repeatPass.classList.add ("match_pass")
-                        }else if (self.pass.value === self.repeatPass.value){
-                            self.pass.classList.remove ("match_pass")
-                            self.repeatPass.classList.remove ("match_pass")
-                            console.log("true")
-                        }
-                 }
+// MODULE
+var app = angular.module("FormValidation",[])
 
 
 
-                },false )
-            }
-        
-            }
+
+//CONTROLLERS 
+
+app.controller("defaultCtrl",['$scope', function($scope) {
+
+this.getError = function (err) {
+    if (angular.isDefined(err)){
+        if(err.required){
+            return "This is required field"
+        }else if (err.email){
+            return "Please enter correct email@email.com"
+        }
     }
+}
 
-    RegistrationForm.prototype.onFocus = function (e) {
-        e.addEventListener("focus", function(){
-     if (this.style.backgroundColor === "pink")
-     this.style.backgroundColor = ""
-        },false )
-      
-    };
-    
-    RegistrationForm.prototype.invalid = function (e) {
-       e.addEventListener("blur", function(){
-         if (this.value.length === 0)
-    this.style.backgroundColor = "pink"
-       },false )
-    
-   
-};
+this.createNewUser = function (data){
+    console.log(data)
+}
+}] )
 
 
-window.app = window.app || {};
-window.app.RegistrationForm = RegistrationForm;
 
 
-})()
-
- 
-
-   
-
-   
